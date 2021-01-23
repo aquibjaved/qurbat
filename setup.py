e92f0b8999c32e776b19942128f07a76e3f0327d
aquibjaved <ajkhan357@gmail.com>
2021-01-23 11:07:13 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='qurbat',
+   version='0.0.1',
+   description='A module for data comparison(Image and Text)',
+   author='Aquib Javed Khan',
+   author_email='aquib_marwan@protonmail.com',
+   packages=['qurbat'],
+   install_requires=['requirements.txt'],
+)
+
